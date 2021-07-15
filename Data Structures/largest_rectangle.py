bffbad0e0c90d97eedb9a70064563b2fad933465
Nguyễn Thành Long <83007241+ngthalogit@users.noreply.github.com>
2021-07-15 10:34:55 +0700

    Create largest_rectangle.py

---
+++ Data Structures/largest_rectangle.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'largestRectangle' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY h as parameter.
+#
+
+def largestRectangle(h):
+    stack = []
+    index = 0
+    rs = 0
+    while index < len(h):
+        if len(stack) == 0 or h[stack[-1]] <= h[index]:
+            stack.append(index)
+            index += 1
+        else:
+            top = stack.pop()
+            if len(stack)==0:
+                s = h[top]*(index)
+            else:
+                s = h[top]*(index-stack[-1]-1)
+            rs = max(rs, s)
+    while len(stack):
+        top = stack.pop()
+        if len(stack)==0:
+            s = h[top]*(index)
+        else:
+            s = h[top]*(index-stack[-1]-1)
+        rs = max(rs, s)
+    return rs
+    """stack = []
+    rs = 0
+    index = 0
+    while index < len(h):
+        if len(stack)==0 or h[stack[-1]] <= h[index]:
+            stack.append(index)
+            index +=1
+        else:
+            top = stack.pop()
+            if len(stack):
+            rs = max(rs, h[top]*(index-stack[-1]-1 if stack else index))
+    
+    while len(stack):
+        top = stack.pop()
+        rs = max(rs, h[top]*(index-stack[-1]-1 if stack else index))
+        
+    return rs"""
+    
+    
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    h = list(map(int, input().rstrip().split()))
+
+    result = largestRectangle(h)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
